cd885ac44aa42371877141811380a9d65eb6ba79
Ali Dastourin <alidastourian@gmail.com>
2024-02-13 12:47:55 +0300

    slider done

--- main.js
+++ main.js
@@ -11,3 +11,43 @@ $('#right').click(function () {
 $('#left').click(function () {
     $('#story-animate').animate({'margin-left': "-=200px"}, 500)
 })
+
+function Slider() {
+    if(nextslid>max){
+        nextslid=0;
+    }
+    if(nextslid<min){
+        nextslid=4;
+    }
+
+    slideritem.hide();
+    slideritem.eq(nextslid).fadeIn(100);
+    nextslid++;
+}
+function Slp() {
+
+    if(nextslid<min){
+        nextslid=4;
+    }
+
+    slideritem.hide();
+    slideritem.eq(nextslid).fadeIn(100);
+    nextslid--;
+}
+var max=4;
+var min=0;
+var slidertag =$('#slider-container')
+var  slideritem=slidertag.find('.item')
+var nextslid=0;
+$('#click-right').click(function (){
+    Slider();
+
+});
+$('#click-left').click(function (){
+    Slp();
+});
+
+Slider();
+var Timer = setInterval(function (){
+    Slider();
+},2500)
